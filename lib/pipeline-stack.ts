7612b2845a0d3105f00dd9c722bc771131402e89
Andrew Ouko <andrew.ouko.254@gmail.com>
2023-12-14 12:16:00 +0300

    added new code pipeline for our repository with install and commands

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,5 +1,6 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Repository } from "aws-cdk-lib/aws-codecommit";
+import { CodeBuildStep, CodePipeline, CodePipelineSource } from "aws-cdk-lib/pipelines";
 import { Construct } from "constructs";
 
 export class WorkshopPipelineStack extends Stack {
@@ -7,8 +8,24 @@ export class WorkshopPipelineStack extends Stack {
         super(scope, id, props);
 
         // create code commit repository
-        new Repository(this, 'CdkWorkshopRepo', {
+        const repository = new Repository(this, 'CdkWorkshopRepo', {
             repositoryName: 'CdkWorkshopRepo',
+        })
+
+        // create pipeline
+        new CodePipeline(this, 'CdkWorkshopPipeline', {
+            pipelineName: 'CdkWorkshopPipeline',
+            synth: new CodeBuildStep('Synthstep', {
+                input: CodePipelineSource.codeCommit(repository, 'master'),
+                installCommands: [
+                    'npm install -g aws-cdk'
+                ],
+                commands: [
+                    'npm ci',
+                    'npm run build',
+                    'npx cdk synth'
+                ]
+            })
         })
     }
 }
