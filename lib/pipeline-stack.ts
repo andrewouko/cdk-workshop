f44c774fe1ea0cef41ff11331fd15a5498693623
Andrew Ouko <andrew.ouko.254@gmail.com>
2023-12-19 10:43:46 +0300

    added authentication from secrets manager

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,39 +1,41 @@
-import { Stack, StackProps } from "aws-cdk-lib";
+import { SecretValue, Stack, StackProps } from "aws-cdk-lib";
 import { Repository } from "aws-cdk-lib/aws-codecommit";
-import { CodeBuildStep, CodePipeline, CodePipelineSource } from "aws-cdk-lib/pipelines";
+import {
+  CodeBuildStep,
+  CodePipeline,
+  CodePipelineSource,
+} from "aws-cdk-lib/pipelines";
 import { Construct } from "constructs";
 import { WorkshopPipelineStage } from "./pipeline-stage";
-import { pipeline } from "stream";
 
 export class WorkshopPipelineStack extends Stack {
-    constructor(scope: Construct, id: string, props?: StackProps){
-        super(scope, id, props);
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
 
-        // create code commit repository
-        const repository = new Repository(this, 'CdkWorkshopRepo', {
-            repositoryName: 'CdkWorkshopRepo',
-        })
+    // create code commit repository
+    const repository = new Repository(this, "CdkWorkshopRepo", {
+      repositoryName: "CdkWorkshopRepo",
+    });
 
-        // create pipeline
-        const pipeline = new CodePipeline(this, 'CdkWorkshopPipeline', {
-            pipelineName: 'CdkWorkshopPipeline',
-            synth: new CodeBuildStep('Synthstep', {
-                // input: CodePipelineSource.codeCommit(repository, 'master'),
-                // use github source
-                input: CodePipelineSource.gitHub('andrewouko/cdk-workshop', 'master'),
-                installCommands: [
-                    'npm install -g aws-cdk'
-                ],
-                commands: [
-                    'npm ci',
-                    'npm run build',
-                    'npx cdk synth'
-                ]
-            })
-        })
+    // create pipeline
+    const pipeline = new CodePipeline(this, "CdkWorkshopPipeline", {
+      pipelineName: "CdkWorkshopPipeline",
+      synth: new CodeBuildStep("Synthstep", {
+        // use CodeCommit source
+        // input: CodePipelineSource.codeCommit(repository, 'master'),
+        // use github source
+        input: CodePipelineSource.gitHub("andrewouko/cdk-workshop", "master", {
+          authentication: SecretValue.secretsManager(
+            "github-access-token-secret"
+          ),
+        }),
+        installCommands: ["npm install -g aws-cdk"],
+        commands: ["npm ci", "npm run build", "npx cdk synth"],
+      }),
+    });
 
-        // add deploy step
-        const deploy = new WorkshopPipelineStage(this, 'Deploy')
-        const deployStage = pipeline.addStage(deploy);
-    }
+    // add deploy step
+    const deploy = new WorkshopPipelineStage(this, "Deploy");
+    const deployStage = pipeline.addStage(deploy);
+  }
 }
