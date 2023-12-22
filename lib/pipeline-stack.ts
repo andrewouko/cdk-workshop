e31afa7f934184315d8f681e0878c6f8f4d73e2d
Andrew Ouko <andrew.ouko.254@gmail.com>
2023-12-22 10:10:42 +0300

    Introduced new currency input @Dalin

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -4,6 +4,7 @@ import {
   CodeBuildStep,
   CodePipeline,
   CodePipelineSource,
+  IFileSetProducer,
 } from "aws-cdk-lib/pipelines";
 import { Construct } from "constructs";
 import { WorkshopPipelineStage } from "./pipeline-stage";
@@ -12,31 +13,34 @@ export class WorkshopPipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // create code commit repository
+    // input (Repository/Source) for the pipeline
+    let input: IFileSetProducer;
+
+    // use CodeCommit source
+    /* // create a new CodeCommit repository
     const repository = new Repository(this, "CdkWorkshopRepo", {
       repositoryName: "CdkWorkshopRepo",
+    });
+    input = CodePipelineSource.codeCommit(repository, "master"); */
+
+    // use a GitHub repository
+    input = CodePipelineSource.gitHub("andrewouko/cdk-workshop", "master", {
+      authentication: SecretValue.secretsManager("github-access-token-secret"),
     });
 
     // create pipeline
     const pipeline = new CodePipeline(this, "CdkWorkshopPipeline", {
       pipelineName: "CdkWorkshopPipeline",
       synth: new CodeBuildStep("Synthstep", {
-        // use CodeCommit source
-        // input: CodePipelineSource.codeCommit(repository, 'master'),
-        // use github source
-        // test if change to Github will trigger pipeline
-        input: CodePipelineSource.gitHub("andrewouko/cdk-workshop", "master", {
-          authentication: SecretValue.secretsManager(
-            "github-access-token-secret"
-          ),
-        }),
+        input,
         installCommands: ["npm install -g aws-cdk"],
         commands: ["npm ci", "npm run build", "npx cdk synth"],
-      }),
+      })
     });
 
-    // add deploy step
+    // add deploy step for the CdkWorkshopStack
     const deploy = new WorkshopPipelineStage(this, "Deploy");
+    // add it to the pipeline
     const deployStage = pipeline.addStage(deploy);
   }
 }
